450262f2d6fab47b1b55346618ac8cb279c143ae
Anderson Lairton <lairton.anderson@gmail.com>
2020-07-22 20:43:56 -0300

    refatorando condição para verificação se o valor é numerico

--- calculadora.php
+++ calculadora.php
@@ -1,12 +1,12 @@
 <?php
-if (is_numeric($_POST['n1']) && is_numeric($_POST['n2'])) {
-    $n1 = $_POST['n1'];
-    $n2 = $_POST['n2'];
-    $operacao = $_POST['acao'];
-} else {
+if (!is_numeric($_POST['n1']) || !is_numeric($_POST['n2'])) {
     echo 'Não foi possivel realizar a operação,foram inseridos caracteres não numericos,por favor refaça a operação';
     die;
 }
+    $n1 = $_POST['n1'];
+    $n2 = $_POST['n2'];
+    $operacao = $_POST['acao'];
+
 switch ($operacao) {
     case 'Soma':
         echo 'A soma de ' . $n1 . ' e ' . $n2 . ' é de ' . ($n1 + $n2);
@@ -17,7 +17,7 @@ switch ($operacao) {
     case 'Divisao':
         if ($n2 == 0) {
             echo 'Divisao invalida,não é possivel dividir o valor de ' . $n1 . ' por zero.Por favor,refaça a operação';
-            die;
+            break;
         } else {
             echo 'A divisão de ' . $n1 . ' e ' . $n2 . ' é de ' . ($n1 / $n2);
         }
@@ -37,6 +37,6 @@ switch ($operacao) {
         }
         break;
     default:
-        # code...
+        echo 'operação invalida';
         break;
 }
